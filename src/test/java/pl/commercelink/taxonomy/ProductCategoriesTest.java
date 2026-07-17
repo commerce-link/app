c15b24884bfb6c64c783242c685832a5a96bd438
SzymonReddigPC <szymon.reddig@pragmaticcoders.com>
2026-07-17 12:45:21 +0200

    Remove commons enums parity guard and dead taxonomy helpers (#110)

--- src/test/java/pl/commercelink/taxonomy/ProductCategoriesTest.java
+++ src/test/java/pl/commercelink/taxonomy/ProductCategoriesTest.java
@@ -23,4 +23,62 @@ class ProductCategoriesTest {
         // when / then
         assertThat(ProductCategories.tryParse(null)).isEmpty();
     }
+
+    @Test
+    void knownCategoriesMatchTheInventoryDictionarySnapshot() {
+        // when / then
+        assertThat(ProductCategories.KNOWN).containsExactlyInAnyOrder(
+                "CPU",
+                "Cooler",
+                "GPU",
+                "Motherboard",
+                "PSU",
+                "Storage",
+                "Memory",
+                "Case",
+                "Fan",
+                "ModdingPC",
+                "Other",
+                "Services",
+                "Laptops",
+                "Desktops",
+                "Workstations",
+                "Servers",
+                "AllInOnePCs",
+                "GraphicsTablets",
+                "Software",
+                "Smartphones",
+                "StationaryPhones",
+                "Tablets",
+                "SmartphoneCases",
+                "ScreenProtectors",
+                "Chargers",
+                "Powerbanks",
+                "MobileHeadphones",
+                "Printers",
+                "LaserPrinters",
+                "InkPrinters",
+                "PhotoPrinters",
+                "LargeFormatPrinters",
+                "LabelPrinters",
+                "Printers3D",
+                "Scanners",
+                "MultifunctionPrinters",
+                "Displays",
+                "Keyboards",
+                "Mice",
+                "KeyboardsAndMice",
+                "Headphones",
+                "Microphones",
+                "Webcams",
+                "Speakers",
+                "MousePads",
+                "GamingChairs",
+                "OfficeChairs",
+                "GamingDesks",
+                "OfficeDesks",
+                "StandingDesks",
+                "MonitorMounts",
+                "Footrests");
+    }
 }
